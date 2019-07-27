89b0ce49abaddbbb0cc0e89d35e213684c485619
Tristan Debrunner <tristan@solana.com>
2019-07-27 12:14:02 -0600

    Disable automatic lalrpop build

--- language/compiler/ir_to_bytecode/syntax/build.rs
+++ language/compiler/ir_to_bytecode/syntax/build.rs
@@ -1,9 +1,13 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+#[cfg(feature = "build-lalrpop")]
 fn main() {
     lalrpop::Configuration::new()
-        .generate_in_source_tree()
+        .use_cargo_dir_conventions()
         .process()
         .unwrap();
 }
+
+#[cfg(not(feature = "build-lalrpop"))]
+fn main() {}
