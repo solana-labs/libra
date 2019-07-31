91939a07353a644efab320b744e7c1ea98fbfd24
Tristan Debrunner <tristan@solana.com>
2019-07-31 14:15:39 -0600

    Move lalrpop generated code to `OUT_DIR`

--- language/compiler/ir_to_bytecode/syntax/build.rs
+++ language/compiler/ir_to_bytecode/syntax/build.rs
@@ -1,13 +1,9 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-#[cfg(feature = "build-lalrpop")]
 fn main() {
     lalrpop::Configuration::new()
         .use_cargo_dir_conventions()
         .process()
         .unwrap();
 }
-
-#[cfg(not(feature = "build-lalrpop"))]
-fn main() {}
