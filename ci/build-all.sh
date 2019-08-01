5bc7b582607d4b1b37449718f6598196f8233839
TristanDebrunner <tristan@solana.com>
2019-08-01 15:23:29 -0600

    Switch tests to Travis CI (#31)

--- ci/build-all.sh
+++ ci/build-all.sh
@@ -2,11 +2,7 @@
 set -e
 cd "$(dirname "$0")/.."
 
-source ci/_
-
-source ci/rust-version.sh nightly
-
 export RUST_BACKTRACE=1
 export RUSTFLAGS="-D warnings"
 
-_ cargo +"$rust_nightly" build --all ${V:+--verbose}
+cargo build --all ${V:+--verbose}
